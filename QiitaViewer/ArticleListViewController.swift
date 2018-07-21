8a51885100e4b21453359518681772976562ce8b
Akito Mizuno <mznakt@gmail.com>
2018-07-22 00:02:40 +0900

    display articles to tableview

--- QiitaViewer/ArticleListViewController.swift
+++ QiitaViewer/ArticleListViewController.swift
@@ -10,10 +10,23 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class ArticleListViewController: UIViewController {
+class ArticleListViewController: UIViewController, UITableViewDataSource {
     let END_POINT: String = "https://qiita.com/api/v2/items"
     let table = UITableView()
     var articles: [[String: String?]] = []
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        let article = articles[indexPath.row]
+        cell.textLabel?.text = article["title"]!
+        cell.detailTextLabel?.text = article["userId"]!
+
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +34,8 @@ class ArticleListViewController: UIViewController {
 
         table.frame = view.frame
         view.addSubview(table)
+        table.dataSource = self
+
         getArticles()
     }
 
@@ -36,9 +51,8 @@ class ArticleListViewController: UIViewController {
                         "userId": json["user"]["id"].string
                     ]
                     self.articles.append(article)
-                    print(article)
                 }
-//                print(self.articles)
+                self.table.reloadData()
 
             case .failure(let error):
                 print(error)
