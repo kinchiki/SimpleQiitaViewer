da44365988e309a0643b2331891ea83ba460976c
Akito Mizuno <mznakt@gmail.com>
2018-07-21 19:13:53 +0900

    create ArticleListViewController and Navigation Controller

---
+++ QiitaViewer/ArticleListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleListViewController.swift
+//  QiitaViewer
+//
+//  Created by 水野陽斗 on 2018/07/21.
+//  Copyright © 2018 akitomizuno. All rights reserved.
+//
+
+import UIKit
+
+class ArticleListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
